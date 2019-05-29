ce4960e5c27568638a57ff7f7943a9389f5fe06a
linln1 <41333235+linln1@users.noreply.github.com>
2019-05-29 22:09:18 +0800

    NN algorithm to fix linear/non-linear functions

---
+++ NN_demo.py
@@ -0,0 +1,34 @@
+#coding=utf-8
+
+import numpy as np
+import tensorflow as tf
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+BATCH_SIZE = 8
+SEED = 23345
+
+rdm = np.random.RandomState(SEED)
+
+X = rdm.rand(32, 2)
+Y_ = [[x1+x2+(rdm.rand()/10.0-0.05)] for (x1, x2) in X]
+
+x = tf.placeholder(tf.float32, shape=(None, 2))
+y_ = tf.placeholder(tf.float32, shape=(None, 1))
+w1 = tf.Variable(tf.random_normal([2, 1], stddev=1, seed=1))
+y = tf.matmul(x, w1)
+
+loss_mse = tf.reduce_mean(tf.where(tf.greater(y_, y), 9*(y_-y), (y-y_)))
+train_step = tf.train.GradientDescentOptimizer(0.001).minimize(loss_mse)
+
+with tf.Session() as sess:
+    init_op = tf.global_variables_initializer()
+    sess.run(init_op)
+    STEPS = 200000
+    for i in range(STEPS):
+        start = (i*BATCH_SIZE) % 32
+        end = (i*BATCH_SIZE) % 32 + BATCH_SIZE
+        sess.run(train_step, feed_dict={x: X[start:end], y_: Y_[start:end]})
+        if i%5000 == 0:
+            print(sess.run(w1))
+    print("final answer is :", sess.run(w1))
