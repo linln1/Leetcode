d7bf69c0848467329173543ade0c6f52295ab8a3
linln1 <41333235+linln1@users.noreply.github.com>
2019-05-30 22:08:16 +0800

    Create mnist_forward_demo.py

---
+++ mnist_forward_demo.py
@@ -0,0 +1,24 @@
+# coding:utf-8
+
+import numpy as py
+import tensorflow as tf
+
+def get_weight(shape, regularizer):
+    w = tf.variable(tf.random_normal(shape,stddev=1,seed=1))
+    if regularizer != None: tf.add_to_collection('loss',tf.contrib.layers.l2_regularizer(regularizer)(w))
+    return w
+
+def get_bias(shape):
+    b = tf.Variable(tf.zero(shape))
+    return b
+
+def forward(x, regularizer):
+    w1 = get_weight([INPUT_NODE, LAYER1_NODE], regularizer)
+    b1 = get_bias([LAYER1_NODE])
+    y1 = tf.nn.relu(tf.matmul(x, w1) + b1)
+
+    w2 = get_weight([LAYER1_NODE, OUTPUT_NODE], regularizer)
+    b2 = get_bias([OUTPUT_NODE])
+    y = tf.nn.relu(tf.matmul(y1, w2) + b2)
+    return y
+
